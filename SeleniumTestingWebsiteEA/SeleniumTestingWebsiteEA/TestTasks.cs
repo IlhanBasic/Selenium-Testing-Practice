2631748cb9561d0947fdab03fa21a343290a2fa7
Ilhan Basic <ilhanbasic456@gmail.com>
2025-01-06 10:30:56 +0100

    init testing project

---
+++ SeleniumTestingWebsiteEA/SeleniumTestingWebsiteEA/TestTasks.cs
@@ -0,0 +1,61 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTestingWebsiteEA
+{
+	public class TestTasks
+	{
+		IWebDriver driver;
+		[SetUp]
+		public void Setup()
+		{
+			driver = new ChromeDriver();
+			driver.Navigate().GoToUrl("http://eaapp.somee.com/");
+		}
+		[Test]
+		public void LoginUser()
+		{
+
+		}
+		[Test]
+		public void IsUserAdmin()
+		{
+
+		}
+		[Test]
+		public void AddEmployee()
+		{
+
+		}
+		[Test]
+		public void RemoveEmployee()
+		{
+
+		}
+		[Test]
+		public void EditEmployee()
+		{
+
+		}
+		[Test]
+		public void SearchEmployee()
+		{
+
+		}
+		[Test]
+		public void ChangeRoleToUser()
+		{
+
+		}
+		[TearDown]
+		public void TearDown()
+		{
+			driver.Close();
+		}
+	}
+}
